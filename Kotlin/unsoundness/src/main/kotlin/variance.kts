ed43ac955fb07103f7c5b8ba5d59c186a7152b61
Jason Dongheng Lee <jason5lee@hotmail.com>
2020-02-18 12:25:13 +0800

    add Kotlin's unsoundness example.

--- Kotlin/unsoundness/src/main/kotlin/variance.kts
+++ Kotlin/unsoundness/src/main/kotlin/variance.kts
@@ -3,9 +3,9 @@ data class Derive<T>(var elem: T) : Base<T>()
 
 val d: Derive<Int> = Derive(0)
 val b: Base<Int> = d
-val ohno: Base<Any> = b
-if (ohno is Derive) {
-    ohno.elem = "ohno"
-    println(ohno.elem) // Output "ohno"
+val a: Base<Any> = b
+if (a is Derive) {
+    a.elem = "hi"
+    println(a.elem)
 }
-println(d.elem) // java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Number
+println(d.elem)
