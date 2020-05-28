3ebb39f29dab7345489efb7ae5eef3b793c641fb
Jason5Lee <jason5lee@hotmail.com>
2020-05-29 07:44:09 +0800

    Fix some problem

--- TypeScript/test-driven-typing/src/index.ts
+++ TypeScript/test-driven-typing/src/index.ts
@@ -1,13 +1,13 @@
-export function is_prime(n: 2.7 | 3.6 | 4.5): false
-export function is_prime(n: 1 | 0 | -1): false
-export function is_prime(n: 2 | 3 | 5 | 7 | 11): true
-export function is_prime(n: 4 | 6 | 8 | 9): false
-export function is_prime(n: number): boolean
-export function is_prime(n: number): boolean {
+export function isPrime(n: 2.7 | 3.6 | 4.5): false
+export function isPrime(n: 1 | 0 | -1): false
+export function isPrime(n: 2 | 3 | 5 | 7 | 11): true
+export function isPrime(n: 4 | 6 | 8 | 9): false
+export function isPrime(n: number): boolean
+export function isPrime(n: number): boolean {
     if (!Number.isInteger(n) || n < 2) {
         return false;
     }
-    let sqrt = Math.sqrt(n);
+    const sqrt = Math.sqrt(n);
     for (let i = 2; i <= sqrt; ++i) {
         if (n % i == 0) {
             return false;
